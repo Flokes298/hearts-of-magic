ce5518ee7547da170281034a7bf91acbb1b52214
Florian Kessels <florian.kessels@alumni.fh-aachen.de>
2025-10-09 13:01:19 +0200

    Datagen setup

--- src/client/java/net/flokes/heartsofmagic/HeartsOfMagicDataGenerator.java
+++ src/client/java/net/flokes/heartsofmagic/HeartsOfMagicDataGenerator.java
@@ -2,10 +2,19 @@ package net.flokes.heartsofmagic;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.flokes.heartsofmagic.datagen.*;
 
 public class HeartsOfMagicDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(ModAdvancementProvider::new);
+        pack.addProvider(ModBlockLootTableProvider::new);
+        pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModChestLootTableProvider::new);
+        pack.addProvider(ModEnglishLanguageProvider::new);
+        pack.addProvider(ModItemTagProvider::new);
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
 	}
 }
